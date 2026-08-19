bd9c8bc81e4793ca0fba94e38f572c57b9ef0884
Emiliano Piragine <piragineemi@gmail.com>
2026-08-19 17:22:20 -0300

    persona y 13 test

--- alumno-typescript/test/alumno.test.ts
+++ alumno-typescript/test/alumno.test.ts
@@ -24,9 +24,9 @@ describe("Alumno", () => {
      expect(alumno.legajo).toBe(123123);
   });
   test("un alumno de 18 años debe ser mayor de edad y debe tener numero de legajo", () => {
-    const alumno = new Alumno("Pedro", 18 , 223123);
+    const alumno = new Alumno("javier", 18 , 223123);
     expect(alumno.esMayorDeEdad()).toBe(true);
-     expect(alumno.legajo).toBe(1235676);
+     expect(alumno.legajo).toBe(223123);
   });
    test("un alumno de 20 años debe ser mayor de edad y debe tener numero de legajo", () => {
     const alumno = new Alumno("Paula", 20 , 1235676);
@@ -34,4 +34,14 @@ describe("Alumno", () => {
     expect(alumno.legajo).toBe(1235676);
   });
 
+    test("un alumno de 22 años debe ser mayor de edad y debe tener numero de legajo", () => {
+    const alumno = new Alumno("Joaquina", 22 , 1265676);
+    expect(alumno.esMayorDeEdad()).toBe(true);
+    expect(alumno.legajo).toBe(1265676);
+})
+  test("un alumno de 21 años debe ser mayor de edad y debe tener numero de legajo", () => {
+    const alumno = new Alumno("julieta", 21 , 2265676);
+    expect(alumno.esMayorDeEdad()).toBe(true);
+    expect(alumno.legajo).toBe(2265676);
+})
 });
--- alumno-typescript/test/profesor.test.ts
+++ alumno-typescript/test/profesor.test.ts
@@ -9,6 +9,17 @@ describe("Profesor", () => {
     expect(profesor.nombre).toBe("Dr. García");
     expect(profesor.legajo).toBe(77707);
   });
-  
 
+   test("un profesor debe tener un nombre y legajo", () => {
+    const profesor = new Profesor("Dr. Ramirez", 20000);
+
+    expect(profesor.nombre).toBe("Dr. Ramirez");
+    expect(profesor.legajo).toBe(20000);
+  });
+test("un profesor debe tener un nombre y legajo", () => {
+    const profesor = new Profesor("Dr. Piragine", 20020);
+
+    expect(profesor.nombre).toBe("Dr. Piragine");
+    expect(profesor.legajo).toBe(20020);
+  });
 });
